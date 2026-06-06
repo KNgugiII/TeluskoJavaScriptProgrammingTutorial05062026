fcc1379f84c45d03dfc2128faf12ef7d26c986f5
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-06 04:40:39 +0300

    Arithmetic Operators

---
+++ ArithmeticOperators.js
@@ -0,0 +1,60 @@
+let varNum001=4
+let varNum002=2
+let varNum003=5
+
+
+/* ADDITION EXAMPLE */
+console.log(varNum001 + varNum002)    
+
+/* SUBTRACTION EXAMPLE */
+console.log(varNum001 - varNum002)
+
+/* MULTIPLICATION EXAMPLE */
+console.log(varNum001 * varNum002)
+
+/* DIVISION EXAMPLE */
+console.log(varNum001 / varNum002)
+
+/* MOD EXAMPLE */
+console.log(varNum003 % varNum002)
+
+/*  Basic JAVASCRIPT ADDITION Math */
+let varNum004=5
+varNum004=varNum004+2
+console.log(varNum004)
+
+/* The above JAVASCRIPT ADDITION math CAN BE BETTER WRITTEN AS BELOW; */
+let varNum004B=5
+varNum004B+=2               /* += INCREMENTS */
+console.log(varNum004B)
+
+/*  Basic JAVASCRIPT SUBTRACTION Math */
+let varNum004C=5
+varNum004C=varNum004C-2
+console.log(varNum004C)
+
+/* The above JAVASCRIPT SUBTRACTION math CAN BE BETTER WRITTEN AS BELOW; */
+let varNum004D=5
+varNum004D-=2               /* -= DECREMENTS */
+console.log(varNum004D)
+
+/* NOTE: There's post and pre increment and post and pre decrement when it comes to the shorthand */
+
+/* CUBE EXAMPLE */
+let varCube=4
+let varResult=(varCube * varCube * varCube)
+
+console.log(varResult)
+
+/* CUBE EXAMPLE SHORTHAND*/
+let varCube001=4
+let varResult001=Math.pow(4,3) /* Implies 4 raised to base 3 */
+
+console.log(varResult001)
+
+/* CUBE EXAMPLE SHORTHAND001*/
+let varCube002=4
+let varResult002= (varCube002** 3) /* DOUBLE ASTERIX implies POWER. While Single implies Multiplication */
+
+console.log(varResult002)
+
