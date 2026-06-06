f7ea6a556c68b05e13f6f6f2d120e4f08af054d4
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-06 05:15:03 +0300

    Relational Operators

---
+++ RelationalOperators.js
@@ -0,0 +1,14 @@
+/*
+These operators are used to compare two values and they include;
+    > greater than,
+    < less than,
+    >= equal to or greater than,
+    <= less than or equal to,
+    === are both values the same.
+*/
+
+/* Javascript can compare numbers and strings, strings and numbers, strings and strings etc hence the need to be extra careful about what relational operator is in us */
+
+/* When comparing numbers and strings, Javascript converts strings to numbers and then does the comparison */
+
+/* When it comes to comparison, the values as well as the data types SHOULD BE THE SAME. The operator to use is === as opposed to the == (double relational operator which implicitly does conversion and then comparison) */
