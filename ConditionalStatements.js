9055a5677500bf28259bbe4ac4a805d711a2a64d
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-08 21:47:19 +0300

    ConditionalStatements

---
+++ ConditionalStatements.js
@@ -0,0 +1,43 @@
+let varNum1=12;
+let varNum2=4;
+
+
+/* Observation: SemiColons are NOT NECESSARY in Javascript but its good practice to include them. */
+
+
+/*------> When you have a single statement, you DO NOT INCLUDE the curly brackets
+if (varNum1B>varNum2B)
+    console.log("varNum1 is greater");  
+else 
+console.log("varNum2 is greater");
+//end if
+*/
+
+let varNum1B=12;
+let varNum2B=24;
+
+/*------> When you have multiple statements, thats when you include the curly brackets so that they can be acted upon in a lumpsome*/
+/*
+if (varNum1B>varNum2B){         
+    console.log("varNum1B is greater");
+    
+}
+else {
+    console.log("varNum2B is greater");
+    console.log("Yeeeiiii");   
+}//end if
+*/
+
+/* How to Test if a Number is Even or ODD */
+
+let varMod=(98);
+let varResult=(varMod % 2);
+
+if (varResult)
+    console.log("Number is ODD");
+else
+    console.log("Number is EVEN");
+/* end if */
+    
+    
+
