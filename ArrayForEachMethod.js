78386f033287db15702256df102e8529ba82080c
KenntheAccessVBADev <ngugikenn@gmail.com>
2026-06-12 04:40:03 +0300

    Array For Each Method

---
+++ ArrayForEachMethod.js
@@ -0,0 +1,9 @@
+/* Prints one element of an array at a time thereby allowing further computations, for instance, multiplying each element by 2 etc */
+
+let arrValues=[36,45,67,88,101]
+
+/* To print each element of the array individually, we'll use the For Each method of the array */
+
+    arrValues.forEach((n) => {
+        console.log(n);
+    });
